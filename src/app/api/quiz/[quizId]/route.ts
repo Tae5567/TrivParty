60676d34c81c9737b158b0014b2d22b6e3d020d1
Tae5567 <eayodej1@umbc.edu>
2025-09-15 18:20:39 +0100

    Fix quizId params build error

--- src/app/api/quiz/[quizId]/route.ts
+++ src/app/api/quiz/[quizId]/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { quizId: string } }
 ) {
   const { quizId } = params;
